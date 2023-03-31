e27ed814cbf0b377f4c2949bf343d57af9991116
Stefanos Apkarian <stefanos.apkarian@gmail.com>
2023-03-31 14:50:35 +0300

    add 3rd hello

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
